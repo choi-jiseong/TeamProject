48d503722d321a41fe420ac93effdbaa2e991e08
choi-jiseong <73868064+choi-jiseong@users.noreply.github.com>
2021-06-06 21:28:13 +0900

    Update StudentQRcode.java

--- app/src/main/java/com/example/teamproject/StudentQRcode.java
+++ app/src/main/java/com/example/teamproject/StudentQRcode.java
@@ -101,7 +101,7 @@ public class StudentQRcode extends Fragment {
     }
     private void token(String data){
 
-        String URL = "http://dlswns619.dothome.co.kr/api/auth";
+        String URL = "";
 
         RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
         StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,  new Response.Listener<String>() {
